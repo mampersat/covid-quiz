bb34964d43af1403c4294b3ef08b1a73177de5c0
Matt <msheppard@gmail.com>
2020-07-28 17:18:41 -0400

    about screen and animations

---
+++ lib/about_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class AboutScreen extends StatefulWidget {
+  @override
+  _AboutScreenState createState() => _AboutScreenState();
+}
+
+class _AboutScreenState extends State<AboutScreen> {
+  void launchURL() async {
+    const url =
+        'https://www.texmed.org/TexasMedicineDetail.aspx?Pageid=46106&id=53977';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('About'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Text(
+              'Simple quiz based on risk list found here:',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            FlatButton(
+              child: Text('Texas Medicine Article'),
+              onPressed: launchURL,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'constants.dart';
+import 'about_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,20 +17,28 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'COVID19 Risk Quiz'),
+      initialRoute: 'home',
+      routes: {
+        'home': (context) => MyHomePage(),
+        'about': (context) => AboutScreen(),
+      },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<Offset> _offsetAnimation;
+  Animation<Offset> _offsetAnimation2;
+
   final random = new Random();
   Risk risk1, risk2, correctRisk;
   int correct = 0, incorrect = 0;
@@ -47,22 +56,26 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void setQuestion() {
-    risk1 = risks[random.nextInt(risks.length)];
-    do {
-      risk2 = risks[random.nextInt(risks.length)];
-    } while (risk1 == risk2 || risk1.value == risk2.value);
+    setState(() {
+      risk1 = risks[random.nextInt(risks.length)];
+      do {
+        risk2 = risks[random.nextInt(risks.length)];
+      } while (risk1 == risk2 || risk1.value == risk2.value);
 
-    correctRisk = risk1.value > risk2.value ? risk1 : risk2;
+      correctRisk = risk1.value > risk2.value ? risk1 : risk2;
+      _controller.reverse();
+    });
   }
 
-  void checkAnswer(risk) {
+  void checkAnswer(risk) async {
     setState(() {
       if (risk == correctRisk) {
         if (!lastGuessWrong) correct++;
         lastGuessWrong = false;
         color1 = Theme.of(context).canvasColor;
         color2 = Theme.of(context).canvasColor;
-        setQuestion();
+        _controller.forward();
+        //setQuestion();
       } else {
         incorrect++;
         if (risk1 == correctRisk)
@@ -76,15 +89,55 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void initState() {
-    setQuestion();
     super.initState();
+
+    _controller = AnimationController(
+        duration: const Duration(seconds: 1), vsync: this); //..repeat();
+
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        print('setting questions');
+        setQuestion();
+      }
+    });
+    _offsetAnimation = Tween<Offset>(
+      begin: Offset.zero,
+      end: const Offset(1.5, 0.0),
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.elasticIn,
+    ));
+
+    _offsetAnimation2 = Tween<Offset>(
+      begin: Offset.zero,
+      end: const Offset(-1.5, 0.0),
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.elasticIn,
+    ));
+
+    setQuestion();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('COVID 19 Risk Quiz'),
+        actions: [
+          FlatButton(
+            child: Text('About'),
+            onPressed: () {
+              Navigator.pushNamed(context, 'about');
+            },
+          )
+        ],
       ),
       body: Center(
         child: Column(
@@ -102,38 +155,44 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Expanded(
               flex: 2,
-              child: Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Card(
-                  elevation: 20.0,
-                  child: FlatButton(
-                    child: Text(risk1.title,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 30.0)),
-                    onPressed: () {
-                      checkAnswer(risk1);
-                    },
-                    color: color1,
+              child: SlideTransition(
+                position: _offsetAnimation,
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Card(
+                    elevation: 20.0,
+                    child: FlatButton(
+                      child: Text(risk1.title,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 30.0)),
+                      onPressed: () {
+                        checkAnswer(risk1);
+                      },
+                      color: color1,
+                    ),
                   ),
                 ),
               ),
             ),
             Expanded(
               flex: 2,
-              child: Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Card(
-                  elevation: 20.0,
-                  child: FlatButton(
-                    child: Text(
-                      risk2.title,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 30.0),
+              child: SlideTransition(
+                position: _offsetAnimation2,
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Card(
+                    elevation: 20.0,
+                    child: FlatButton(
+                      child: Text(
+                        risk2.title,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 30.0),
+                      ),
+                      color: color2,
+                      onPressed: () {
+                        checkAnswer(risk2);
+                      },
                     ),
-                    color: color2,
-                    onPressed: () {
-                      checkAnswer(risk2);
-                    },
                   ),
                 ),
               ),
